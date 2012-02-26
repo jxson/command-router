b55d0160ecb8e414ab39b7f012564104983d094d
Jason Campbell <jason@greatergood.cc>
2012-02-26 14:17:38 -0800

    making sure the module exports the CLI class constructor

--- test/command-router/index.js
+++ test/command-router/index.js
@@ -5,9 +5,23 @@ var assert = require('../assert')
 describe('cli', function(){
   it('should be a CLI instance', function(){
     assert.isObject(cli, 'cli is NOT an object');
-    assert.isFunction(cli.command, 'cli.command() is NOT a method');
-    assert.isFunction(cli.option, 'cli.option() is NOT a method');
-    assert.isFunction(cli.parse, 'cli.parse() is NOT a method');
-    assert.isFunction(cli.dispatch, 'cli.dispatch() is NOT a method');
+    assert.isFunction(cli.command, 'command is NOT a method');
+    assert.isFunction(cli.option, 'option is NOT a method');
+    assert.isFunction(cli.parse, 'parse is NOT a method');
+    assert.isFunction(cli.dispatch, 'dispatch is NOT a method');
+  });
+
+  it('should have a CLI class constructor', function(){
+    var instance
+    ;
+
+    assert.equal(typeof cli.CLI, 'function');
+
+    instance = new cli.CLI();
+
+    assert.isFunction(instance.command, 'command is NOT a method');
+    assert.isFunction(instance.option, 'option is NOT a method');
+    assert.isFunction(instance.parse, 'parse is NOT a method');
+    assert.isFunction(instance.dispatch, 'dispatch is NOT a method');
   });
 }); // describe('cli', ...
