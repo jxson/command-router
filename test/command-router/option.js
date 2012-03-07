31014d83f3833d5a48e8cd51030df5cdd86a41dc
Jason Campbell <jason@greatergood.cc>
2012-03-06 19:58:50 -0800

    making sure the options get handled and defaulted properly

--- test/command-router/option.js
+++ test/command-router/option.js
@@ -11,37 +11,54 @@ describe('cli.option(params)', function(){
   });
 
   describe('without params', function(){
+    beforeEach(function(){
+      cli.option('verbose');
+    });
+
     it('parses option as a boolean ', function(){
-      cli.option('verbose').parse(['haiku', '--verbose']);
+      cli.parse(['haiku', '--verbose']);
 
       assert.equal(cli.options.verbose, true, 'NOT boolean');
     });
 
     it('parses option with a default value of false', function(){
-      cli.option('verbose').parse(['haiku']);
+      cli.parse(['haiku']);
 
       assert.equal(cli.options.verbose, false, 'wrong default');
     });
   }); // describe('without params', ...
 
-  // describe('with valid params', function(){
-  //   it('adds a new option with the correct properties', function(){
-  //     cli.option('config', { alias: 'c'
-  //     , default: '.haiku/config.js'
-  //     , type: path
-  //     });
-  //
-  //     var option = cli.options_[0]
-  //     ;
-  //
-  //     console.log('option', option);
-  //
-  //     assert.equal(option.name, 'config', 'option.name is NOT boolean');
-  //   });
-  //
+  describe('with valid params', function(){
+    beforeEach(function(){
+      cli.option('config', { alias: 'c'
+      , default: '.haiku/config.js'
+      , type: path
+      });
+    });
+
+    it('applies the proper default', function(){
+      var path_ = path.resolve('.haiku/config.js')
+      ;
+
+      cli.parse(['haiku']);
+
+      assert.ok(cli.options.config, 'Missing option');
+      assert.equal(cli.options.config, path_, 'Bad default');
+    });
+
+    it('accepts the alias', function(){
+      var path_ = path.resolve('foo')
+      ;
+
+      cli.parse(['haiku', '-c', 'foo']);
+
+      assert.ok(cli.options.config, 'Missing option');
+      assert.equal(cli.options.config, path_, 'Bad default');
+    });
+
   //   describe('with a name argument in the params', function(){
   //
   //   }); // describe('with a name argument in the params', ...
-  // }); // describe('with valid params', ...
+  }); // describe('with valid params', ...
 }); // describe('.option(params)', ...
 
